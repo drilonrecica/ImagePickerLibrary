d48adc61a98814731a2bd739f1c4fd8e657eaeda
Drilon Recica <dreex@adorsys.de>
2016-03-25 21:00:15 +0100

    Constants for colors, used for buttons

--- src/main/java/com/reqica/drilon/imagepickerlibrary/CONSTANTS.java
+++ src/main/java/com/reqica/drilon/imagepickerlibrary/CONSTANTS.java
@@ -6,4 +6,15 @@ public class CONSTANTS {
     public static final int RESULT_GALLERY_IMAGE = 20;
     public static final int REQUEST_CODE_ASK_PERMISSIONS = 123;
 
+    public static final String BLUE = "BLUE";
+    public static final String RED = "RED";
+    public static final String PINK = "PINK";
+    public static final String PURPLE = "PURPLE";
+    public static final String TEAL = "TEAL";
+    public static final String GREEN = "GREEN";
+    public static final String YELLOW = "YELLOW";
+    public static final String ORANGE = "ORANGE";
+    public static final String BROWN = "BROWN";
+    public static final String BLUE_GREY = "BLUEGREY";
+
 }
