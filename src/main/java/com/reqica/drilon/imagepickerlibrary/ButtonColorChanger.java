83ac45ed7ee08e9e0844dd11c9dadfb8a9b8a3bb
Drilon Recica <dreex@adorsys.de>
2016-03-25 21:14:02 +0100

    Added MIT License

--- src/main/java/com/reqica/drilon/imagepickerlibrary/ButtonColorChanger.java
+++ src/main/java/com/reqica/drilon/imagepickerlibrary/ButtonColorChanger.java
@@ -1,3 +1,28 @@
+/*
+        MIT License
+
+        Copyright (c) [year] [fullname]
+
+        Permission is hereby granted, free of charge, to any person obtaining a copy
+        of this software and associated documentation files (the "Software"), to deal
+        in the Software without restriction, including without limitation the rights
+        to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+        copies of the Software, and to permit persons to whom the Software is
+        furnished to do so, subject to the following conditions:
+
+        The above copyright notice and this permission notice shall be included in all
+        copies or substantial portions of the Software.
+
+        THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+        IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+        FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+        AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+        LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+        OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+        SOFTWARE.
+
+*/
+
 package com.reqica.drilon.imagepickerlibrary;
 
 import android.widget.Button;
@@ -6,12 +31,16 @@ public class ButtonColorChanger {
 
     public static void setButtonColor (String buttonColor , Button b1 , String textColor){
 
-        if (textColor.equals("WHITE")){
-            b1.setTextColor(0xFFFFFF);
-        }else if (textColor.equals("BLACK")){
-            b1.setTextColor(0x000000);
-        }else {
-            b1.setTextColor(0xFFFFFF);
+        switch (textColor) {
+            case "WHITE":
+                b1.setTextColor(0xFFFFFF);
+                break;
+            case "BLACK":
+                b1.setTextColor(0x000000);
+                break;
+            default:
+                b1.setTextColor(0xFFFFFF);
+                break;
         }
 
         switch (buttonColor) {
